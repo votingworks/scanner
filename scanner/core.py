0bc85d9138cccdd06d864cbc1e1c3eb095d6e3a8
Ben Adida <ben@adida.net>
2019-03-25 22:17:11 +0000

    modularized a bit

--- scanner/core.py
+++ scanner/core.py
@@ -6,33 +6,42 @@ import pytesseract
 import json
 import glob
 
-election = json.loads(open("./election.json", "r").read())
 
-simplified_contests = [{"title": c['title'], "answers": [a['name'] for a in c['candidates']]} for c in election['contests']]
+def get_contests(election):
+    simplified_contests = [{"title": c['title'], "answers": [a['name'] for a in c['candidates']]} for c in election['contests']]
 
-print(simplified_contests)
+    return simplified_contests
 
-# loop through ballots
-files = glob.glob("./ballots/batch-1/*.jpg")
-
-print(files)
-
-# Simple image to string
-for f in files:
-    raw_ballot = pytesseract.image_to_string(Image.open(f))
+def convert_ballot_image(contests, image):
+    raw_ballot = pytesseract.image_to_string(image)
 
     position = 0
 
     lines = raw_ballot.split("\n")
     for line_num in range(len(lines)):
         line = lines[line_num]
-        if line == simplified_contests[position]['title']:
+        if line == contests[position]['title']:
             option = lines[line_num+1]
             print(option)
             position += 1
 
-        if position >= len(simplified_contests):
+        if position >= len(contests):
             break
+    
+def process_directory(contests, directory_path):
+    files = glob.glob(directory_path + "/*.jpg")
+    # Simple image to string
+    for f in files:
+        convert_ballot_image(contests, Image.open(f))
+        print("\n\n")
 
-    print("\n\n")
+        
+def main():
+    election = json.loads(open("./election.json", "r").read())
+    contests = get_contests(election)
+    process_directory(contests, "./ballots/batch-1")
+
+main()
+
+
         
