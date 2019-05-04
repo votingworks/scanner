5ac2f12fe6f99535a2e5af630965529c09e03ad6
Ben Adida <ben@adida.net>
2019-05-04 03:33:14 +0000

    QR code scanning working

--- src/scanner.ts
+++ src/scanner.ts
@@ -13,15 +13,34 @@ const dir = fs.readdirSync(input_path)
 const election_path = path.join(__dirname, '..', 'electionSample.json')
 const election = JSON.parse(fs.readFileSync(election_path))
 
+// @ts-ignore image type not defined
+const scan = (im) => {
+  // look at rough regions where the QR code is expected
+  
+  const [width, height] = im.sizes
+
+  const firstImage = im.crop({x: width*3/5, width: width/5, y:0, height: height/4})
+  const firstScan = jsQR(firstImage.data, firstImage.width, firstImage.height)
+
+  if (firstScan) {
+    return firstScan
+  }
+  
+  const secondImage = im.crop({x: width/5, width: width/5, y:height*3/4, height: height/4})
+  const secondScan = jsQR(secondImage.data, secondImage.width, secondImage.height)
+  return secondScan
+}
+
 dir.forEach((f : string) => {
   const file = path.join(input_path, f)
   // @ts-ignore image type not defined
   ImageJS.load(file).then(function(im) {
-    console.log(file)
-    const qrData : string = jsQR(im.data, im.width, im.height).data as string
+    const scanResult = scan(im)
+    
+    const qrData : string = scanResult.data as string
 
-    const [ballotStyleId, precinctId, choices] = qrData.split(".")
-    console.log(choices)
+    const [ballotStyleId, precinctId, allChoices] = qrData.split(".")
+    console.log(allChoices)
     // figure out the contests
     const ballotStyle = election.ballotStyles.find((b : BallotStyle)=> b.id === ballotStyleId)
     const contests : Contests = election.contests.filter((c : Contest) => ballotStyle.districts.includes(c.districtId))
@@ -29,26 +48,23 @@ dir.forEach((f : string) => {
     // prepare the CVR
     let votes : Ballot = {precinctId}
 
-    const choices_list = choices.split("/")
-    console.log("LENGTH", choices_list.length, contests.length)
+    const allChoicesList = allChoices.split("/")
     contests.forEach((c : Contest, contest_num : number) => {
       if (c.type === "yesno") {
 
       }
 
       if (c.type === "candidate") {
-	const choice = choices_list[contest_num]
-	console.log(c.id, (c as CandidateContest).candidates.length)
-	if (choice !== '') {
-	  console.log("CHOICE", choice, contest_num)
-	  votes[c.id] = (c as CandidateContest).candidates[parseInt(choice)].id
+	// choices for this question
+	const choices = allChoicesList[contest_num].split(",")
+	if (choices.length > 1 || choices[0] !== '') {
+	  votes[c.id] = choices.map(choice => (c as CandidateContest).candidates[parseInt(choice)].id)
 	}
       }
     })
 
     console.log(precinctId, votes)
 
-    
     // put CVR back together from QR code
     
     // output the CVR
